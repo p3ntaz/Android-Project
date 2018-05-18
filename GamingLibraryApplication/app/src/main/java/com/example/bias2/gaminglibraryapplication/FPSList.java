4a42cbc0c0ffad538db402e60868dd5677dda02a
bias2402 <bias2402@hotmail.dk>
2018-05-18 16:49:03 +0200

    Activies and work with fragment

---
+++ GamingLibraryApplication/app/src/main/java/com/example/bias2/gaminglibraryapplication/FPSList.java
@@ -0,0 +1,22 @@
+package com.example.bias2.gaminglibraryapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FPSList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fpslist);
+        Setup();
+    }
+
+    void Setup() {
+        Fragment frag = new TextFragment();
+        FragmentManager fragMng = getSupportFragmentManager();
+        fragMng.beginTransaction().add(R.id.fragFrame, frag).addToBackStack(null).commit();
+    }
+}
---
+++ GamingLibraryApplication/app/src/main/java/com/example/bias2/gaminglibraryapplication/TextFragment.java
@@ -0,0 +1,27 @@
+package com.example.bias2.gaminglibraryapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
