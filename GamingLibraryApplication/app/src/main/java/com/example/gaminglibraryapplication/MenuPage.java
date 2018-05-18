02b64312a0304545491fcb0a1fc23aaf8bc9ec92
giffel <giffel123@gmail.com>
2018-05-18 16:53:01 +0200

    lo

--- GamingLibraryApplication/app/src/main/java/com/example/gaminglibraryapplication/MenuPage.java
+++ GamingLibraryApplication/app/src/main/java/com/example/gaminglibraryapplication/MenuPage.java
@@ -1,12 +1,12 @@
 package com.example.gaminglibraryapplication;
 
-import android.support.test.InstrumentationRegistry;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.android.volley.VolleyError;
 import com.igdb.api_android_java.model.APIWrapper;
@@ -16,43 +16,78 @@ import com.igdb.api_android_java.callback.onSuccessCallback;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 import static junit.framework.Assert.fail;
 
-public class MenuPage extends AppCompatActivity {
+public class MenuPage extends AppCompatActivity  {
 
     private APIWrapper wrapper;
-    private TextView newTextView;
-    private String Gameinfo;
+    private List<String> arrayList =new ArrayList<>();
+    private ListView lv;
+    private String GamesIDs;
+    private String NewGamesIDs[];
+    private String NewGamesIDs2 = "";
+    private String Gamelist;
+    private  ArrayAdapter<String> adapter;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_page);
+        lv = (ListView) findViewById(R.id.Listview);
         SetUpButton();
-        newTextView = (TextView) findViewById(R.id.textView2);
-        newTextView.setText(R.string.my_love_text);
+
+        try {
+            Genres();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setUp() {
         wrapper = new APIWrapper(getApplicationContext(), "c957fd42f03e55db16b0d7ad5c74a2d5");
     }
 
-    public void testSingleGames() throws InterruptedException {
+    public void Genres() throws InterruptedException {
         setUp();
         Parameters params = new Parameters()
-                .addIds("1942")
-                .addFields("screenshots");
+                .addIds("7");
 
-        wrapper.games(params, new onSuccessCallback() {
+        wrapper.search(APIWrapper.Endpoint.GENRES, params, new onSuccessCallback(){
             @Override
             public void onSuccess(JSONArray result) {
-                // Do something with resulting JSONArray
-                Log.d("tesst", "result: " + result.toString());
-            }
 
+                try {
+                    JSONObject Genre = result.getJSONObject(0);
+                    GamesIDs = Genre.getString("games");
+                    String regex = "\\[|\\]";
+                    GamesIDs = GamesIDs.replaceAll(regex, "");
+                    NewGamesIDs = GamesIDs.split(",");
+
+
+                    for (int i=0; i<50; i++)
+                    {
+                        NewGamesIDs2 += NewGamesIDs[i] + ",";
+                    }
+
+                    NewGamesIDs2 = NewGamesIDs2.substring(0, NewGamesIDs2.length() - 1);
+
+                    Log.d("teest", "result:" + GamesIDs);
+                    Log.d("teest2", "result:" + GamesIDs.length());
+                    Log.d("teest3", "result:" + NewGamesIDs[0]);
+                    Log.d("teest4", "result:" + NewGamesIDs.length);
+                    Log.d("teest5", "result:" +  NewGamesIDs2);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    fail("JSONExeption!!");
+                }
+            }
             @Override
             public void onError(VolleyError error) {
                 Log.e("tesst", "error: " + error);
@@ -60,32 +95,51 @@ public class MenuPage extends AppCompatActivity {
         });
     }
 
-    public void testSingleGames2() throws InterruptedException {
+    public void Games() throws InterruptedException {
         setUp();
         Parameters params = new Parameters()
-                .addSearch("Atari")
-                .addFields("name")
-                .addLimit("2");
+                .addIds( NewGamesIDs2)
+                .addFields("name,popularity");
 
-        wrapper.games(params, new onSuccessCallback() {
+        wrapper.games(params, new onSuccessCallback(){
             @Override
             public void onSuccess(JSONArray result) {
-                // Do something with resulting JSONArray
-                Log.d("tesst", "result: " + result.toString());
-                Gameinfo = result.toString();
-            }
 
+                try {
+                    arrayList = new ArrayList(result.length());
+
+                    for (int e=0; e<result.length(); e++)
+                    {
+                        JSONObject game = result.getJSONObject(e);
+                        Gamelist = "Navn: " + game.getString("name") + " Release date: " + game.getString("popularity");
+
+                        arrayList.add(Gamelist);
+
+                    }
+
+                    Log.d("test", "result: " + arrayList);
+                    Log.d("test2", "result: " + Gamelist);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    fail("JSONExeption!!");
+                }
+            }
             @Override
             public void onError(VolleyError error) {
                 Log.e("tesst", "error: " + error);
             }
         });
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        lv.setAdapter(adapter);
     }
 
     void ButtonClicked() throws InterruptedException {
-        testSingleGames();
-        testSingleGames2();
-        newTextView.setText(Gameinfo);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        lv.setAdapter(adapter);
+        Games();
         Log.e("test", "hej");
     }
 
